0a9d6d8488ff3dc59530e4d806b6681e65b2aaf8
JayBatra14 <jatin.batra92@gmail.com>
2022-08-20 18:17:39 +0530

    initial commit

---
+++ clinic/build/precache-manifest.e9c5f18733c992608575b4f5a4ac483a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87ae03c9076adcfaee8b09c9a7d2904",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e6c11851aeb5533368c",
+    "url": "/static/css/2.1ebb0010.chunk.css"
+  },
+  {
+    "revision": "fe092b8cbf348765a928",
+    "url": "/static/css/main.2a3b5f72.chunk.css"
+  },
+  {
+    "revision": "7e6c11851aeb5533368c",
+    "url": "/static/js/2.54968961.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.54968961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe092b8cbf348765a928",
+    "url": "/static/js/main.8f598998.chunk.js"
+  },
+  {
+    "revision": "202c9905d54f7da668e0",
+    "url": "/static/js/runtime-main.73729c72.js"
+  }
+]);
